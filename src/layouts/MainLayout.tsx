257bbb659fdad3416df8df78727a241414230d75
SkyLightQP <combbm@naver.com>
2020-01-17 21:43:41 +0900

    fix: remove snowstorm

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-// @ts-ignore
-import SnowStorm from 'react-snowstorm';
 import Footer from '../components/Footer';
 import Background from '../utils/Background';
 import CenterContainer from '../utils/ContainerUtils/CenterContainer';
@@ -19,9 +17,6 @@ const MainLayout: React.FC = ({ children }) => {
                 {children}
             </CenterContainer>
 
-            <SnowStorm floflakesMax={80} flakesMaxActive={30} snowColor="#2c4365" vMaxX={0}/>
-            <SnowStorm flakesMax={80} flakesMaxActive={30} snowColor="#676583" vMaxX={0}/>
-            <SnowStorm flakesMax={80} flakesMaxActive={30} snowColor="#8a95b0" vMaxX={0}/>
             <Footer/>
             <Background color="#f1f2f6"/>
         </WrapperStyle>
