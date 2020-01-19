790e2606d301fecebaff96ad253bab0656057f5c
SkyLightQP <combbm@naver.com>
2020-01-19 20:07:48 +0900

    style: apply lint style

--- src/utils/MobileLineBreak.tsx
+++ src/utils/MobileLineBreak.tsx
@@ -3,12 +3,12 @@ import styled from 'styled-components';
 
 const LineBreakStyle = styled.br`
     display: none;
-    
+
     @media screen and (max-width: 420px) {
         display: inline;
-    }    
+    }
 `;
 
-const MobileLineBreak: React.FC = () => (<LineBreakStyle/>);
+const MobileLineBreak: React.FC = () => <LineBreakStyle />;
 
 export default MobileLineBreak;
