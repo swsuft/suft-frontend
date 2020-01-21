757eed68b8f0a15f2389eac655045f26c7b5aee5
SkyLightQP <combbm@naver.com>
2020-01-21 22:37:07 +0900

    feat: add Back icon atomic

---
+++ src/atomics/icons/Back/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+const BackStyle = styled(FontAwesomeIcon)`
+    color: var(--color-gray-text);
+    
+    &:hover {
+        color: var(--color-gray-background);
+        cursor: pointer;
+    }
+`;
+
+interface BackProps {
+    readonly onClick?: React.MouseEventHandler;
+}
+
+const Back: React.FC<BackProps> = ({ onClick }) => {
+    return <BackStyle icon={faTimes} onClick={onClick} />;
+};
+
+export default Back;
