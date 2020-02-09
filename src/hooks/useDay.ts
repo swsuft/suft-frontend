fd6c52757cd5de578a2f618cff0f86b28b919463
SkyLightQP <combbm@naver.com>
2020-02-09 18:38:28 +0900

    fix: add useEffect() in useDay hooks

--- src/hooks/useDay.ts
+++ src/hooks/useDay.ts
@@ -1,15 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const useDay = () => {
     const [today, setToday] = useState<number>(0);
     const [tomorrow, setTomorrow] = useState<number>(0);
 
-    const todayDate = new Date();
-    const tomorrowDate = new Date();
-    tomorrowDate.setDate(tomorrowDate.getDate() + 1);
+    useEffect(() => {
+        const todayDate = new Date();
+        const tomorrowDate = new Date();
+        tomorrowDate.setDate(tomorrowDate.getDate() + 1);
 
-    setToday(todayDate.getDate());
-    setTomorrow(tomorrowDate.getDate());
+        setToday(todayDate.getDate());
+        setTomorrow(tomorrowDate.getDate());
+    }, []);
 
     return [today, tomorrow];
 };
