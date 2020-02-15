906f23ba3d76d8c6e80cde938ae0f5a640163612
SkyLightQP <combbm@naver.com>
2020-02-15 18:22:12 +0900

    refactor: fix sidebar render logic

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
 import Container from '../../utils/ContainerUtils/Container';
 import AdminLayout from '../../layouts/AdminLayout';
-import ProblemTable from '../../components/Admin/ProblemTable';
 import AdminMenuStatus, { AdminMenuStatusType } from '../../constants/AdminMenuStatus';
-import UserTable from '../../components/Admin/UserTable';
 import CreateEditor from '../../components/DraftEditor/CreateEditor';
+import ProblemTable from '../../components/Admin/ProblemTable';
+import UserTable from '../../components/Admin/UserTable';
 
 const TitleStyle = styled.h1`
     margin: 1rem auto;
@@ -19,49 +19,42 @@ const TextStyle = styled.span`
 const Admin: React.FC = () => {
     const [nowMenu, setNowMenu] = useState<AdminMenuStatusType>(AdminMenuStatus.ADMIN_PROBLEM);
 
-    switch (nowMenu) {
-        case AdminMenuStatus.ADMIN_VIEW:
-            return (
-                <AdminLayout setNowMenu={setNowMenu}>
-                    <Container>
-                        <TitleStyle>문제 관리</TitleStyle>
-
-                        <ProblemTable />
-                    </Container>
-                </AdminLayout>
-            );
-        case AdminMenuStatus.ADMIN_PROBLEM:
-            return (
-                <AdminLayout setNowMenu={setNowMenu}>
-                    <Container>
+    const renderBody = useCallback(() => {
+        switch (nowMenu) {
+            case AdminMenuStatus.ADMIN_PROBLEM:
+                return (
+                    <>
                         <TitleStyle>
                             문제 등록 <TextStyle>자격증은 학년, 학기 기타 카테고리입니다.</TextStyle>
                         </TitleStyle>
-
                         <CreateEditor />
                         <br />
-                    </Container>
-                </AdminLayout>
-            );
-        case AdminMenuStatus.ADMIN_USERBLOCK:
-            return (
-                <AdminLayout setNowMenu={setNowMenu}>
-                    <Container>
+                    </>
+                );
+            case AdminMenuStatus.ADMIN_VIEW:
+                return (
+                    <>
+                        <TitleStyle>문제 관리</TitleStyle>
+                        <ProblemTable />
+                    </>
+                );
+            case AdminMenuStatus.ADMIN_USERBLOCK:
+                return (
+                    <>
                         <TitleStyle>유저 차단 설정</TitleStyle>
-
                         <UserTable />
-                    </Container>
-                </AdminLayout>
-            );
-        default:
-            return (
-                <AdminLayout setNowMenu={setNowMenu}>
-                    <Container>
-                        <p>AdminMenuStatus Error</p>
-                    </Container>
-                </AdminLayout>
-            );
-    }
+                    </>
+                );
+            default:
+                return <p>AdminMenuStatus Error</p>;
+        }
+    }, [nowMenu]);
+
+    return (
+        <AdminLayout setNowMenu={setNowMenu}>
+            <Container>{renderBody()}</Container>
+        </AdminLayout>
+    );
 };
 
 export default Admin;
