16f397a794f1048507515461097ac862f500dfad
SkyLightQP <combbm@naver.com>
2020-02-22 17:01:27 +0900

    fix: change font size and text

--- src/components/CbtAnswer/index.tsx
+++ src/components/CbtAnswer/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { MutableRefObject } from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
@@ -33,10 +33,14 @@ const ButtonStyle = styled.button`
     @media screen and (max-width: 420px) {
         border-radius: 10px;
     }
+
+    &:hover {
+        color: white;
+    }
 `;
 
 const EtcTextStyle = styled.p`
-    font-size: 12px;
+    font-size: 14px;
     color: var(--color-text);
 `;
 
@@ -45,16 +49,17 @@ interface CbtAnswerProps {
     readonly onChange?: React.ChangeEventHandler;
     readonly onKeyPress?: React.KeyboardEventHandler;
     readonly onButtonClick?: React.MouseEventHandler;
+    readonly inputRef?: MutableRefObject<HTMLInputElement | null>;
 }
 
-const CbtAnswer: React.FC<CbtAnswerProps> = ({ answerValue, onChange, onKeyPress, onButtonClick }) => {
+const CbtAnswer: React.FC<CbtAnswerProps> = ({ answerValue, onChange, onKeyPress, onButtonClick, inputRef }) => {
     return (
         <AnswerContainer>
-            <InputStyle value={answerValue} autoFocus placeholder="정답을 입력해주세요." onChange={onChange} onKeyPress={onKeyPress} />
+            <InputStyle ref={inputRef} value={answerValue} autoFocus placeholder="정답을 입력해주세요." onChange={onChange} onKeyPress={onKeyPress} />
             <ButtonStyle type="button" onClick={onButtonClick}>
                 <FontAwesomeIcon icon={faPaperPlane} /> 제출
             </ButtonStyle>
-            <EtcTextStyle>* 숫자 버튼을 이용하거나 입력칸을 이용하여 정답을 제출 할 수 있습니다.</EtcTextStyle>
+            <EtcTextStyle>* 숫자 버튼을 통해서도 정답 제출이 가능합니다.</EtcTextStyle>
         </AnswerContainer>
     );
 };
