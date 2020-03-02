5efe45e4d7f8e58f9bc11fa52bb534640424b281
SkyLightQP <combbm@naver.com>
2020-03-02 16:31:44 +0900

    fix: update header text

--- src/components/Login/LoginHeaderText.tsx
+++ src/components/Login/LoginHeaderText.tsx
@@ -22,7 +22,7 @@ const MainTextStyle = styled.p`
 
 const LoginHeaderText: React.FC = () => (
     <>
-        <SubTextStyle>시험 대비 CBT 문제풀이 서비스</SubTextStyle>
+        <SubTextStyle>시험 대비 문제풀이 서비스</SubTextStyle>
         <MainTextStyle>수프트</MainTextStyle>
     </>
 );
