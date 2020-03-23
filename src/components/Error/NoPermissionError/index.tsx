51cab36b546d7c2c76f4077299d22a804df51334
SkyLightQP <combbm@naver.com>
2020-03-23 23:10:59 +0900

    fix: fix design of NoPermissionError

--- src/components/Error/NoPermissionError/index.tsx
+++ src/components/Error/NoPermissionError/index.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShieldAlt } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import CenterContainer from '../../../utils/ContainerUtils/CenterContainer';
 
 const WarningStyle = styled.div`
     text-align: center;
 `;
 
+const ErrorTitleStyle = styled.span`
+    font-size: 2em;
+    font-weight: bold;
+    color: white;
+    background-color: var(--color-red);
+    padding: 0 4px;
+`;
+
 const NoPermissionError: React.FC = () => {
     return (
         <CenterContainer>
             <WarningStyle>
-                <FontAwesomeIcon icon={faShieldAlt} size="10x" />
+                <FontAwesomeIcon icon={faTimes} size="10x" />
                 <br />
                 <br />
-                <h1>이걸 보고 있다면 당신은 접근 권한이 없어요!</h1>
-                <h4>충분한 권한이 있는데 이 페이지가 표시된다면 하단 문의하기를 통해 알려주세요.</h4>
+                <ErrorTitleStyle>403 권한없음!</ErrorTitleStyle>
+                <h1>해당 페이지에 접근할 권한이 없어요</h1>
+                <h3>로그인이 되어있지 않거나 관리자가 아니에요</h3>
             </WarningStyle>
         </CenterContainer>
     );
