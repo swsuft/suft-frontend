504c69d6094a6a8898c3c822b3ecf06d423f0f1b
SkyLightQP <combbm@naver.com>
2020-03-28 23:07:02 +0900

    fix: add `ErrorContainer` style in error components
    - fix: remove pages/NotFound.tsx file

--- src/components/Error/NoPermissionError/index.tsx
+++ src/components/Error/NoPermissionError/index.tsx
@@ -4,6 +4,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import CenterContainer from '../../../utils/ContainerUtils/CenterContainer';
 import ErrorTitle from '../../../atomics/Typography/ErrorTitle';
+import Footer from '../../Footer';
+
+const ErrorContainer = styled.div`
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+    width: 100%;
+`;
 
 const WarningStyle = styled.div`
     text-align: center;
@@ -11,16 +19,19 @@ const WarningStyle = styled.div`
 
 const NoPermissionError: React.FC = () => {
     return (
-        <CenterContainer>
-            <WarningStyle>
-                <FontAwesomeIcon icon={faTimes} size="10x" />
-                <br />
-                <br />
-                <ErrorTitle>403 권한없음!</ErrorTitle>
-                <h1>해당 페이지에 접근할 권한이 없어요</h1>
-                <h3>로그인이 되어있지 않거나 관리자가 아니에요</h3>
-            </WarningStyle>
-        </CenterContainer>
+        <ErrorContainer>
+            <CenterContainer>
+                <WarningStyle>
+                    <FontAwesomeIcon icon={faTimes} size="10x" />
+                    <br />
+                    <br />
+                    <ErrorTitle>403 권한없음!</ErrorTitle>
+                    <h1>해당 페이지에 접근할 권한이 없어요</h1>
+                    <h3>관리자만 접근할 수 있는 페이지에요</h3>
+                </WarningStyle>
+            </CenterContainer>
+            <Footer />
+        </ErrorContainer>
     );
 };
 
--- src/components/Error/NotFoundError/index.tsx
+++ src/components/Error/NotFoundError/index.tsx
@@ -4,24 +4,34 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faQuestion } from '@fortawesome/free-solid-svg-icons';
 import CenterContainer from '../../../utils/ContainerUtils/CenterContainer';
 import ErrorTitle from '../../../atomics/Typography/ErrorTitle';
+import Footer from '../../Footer';
+
+const ErrorContainer = styled.div`
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+    width: 100%;
+`;
 
 const WarningStyle = styled.div`
     text-align: center;
 `;
 
 const NotFoundError: React.FC = () => {
-    // Legacy-NotFoundError Page
     return (
-        <CenterContainer>
-            <WarningStyle>
-                <FontAwesomeIcon icon={faQuestion} size="10x" />
-                <br />
-                <br />
-                <ErrorTitle>404 페이지없음!</ErrorTitle>
-                <h1>페이지를 찾을 수 없어요</h1>
-                <h3>주소가 잘못되거나 존재하지 않는 페이지이에요</h3>
-            </WarningStyle>
-        </CenterContainer>
+        <ErrorContainer>
+            <CenterContainer>
+                <WarningStyle>
+                    <FontAwesomeIcon icon={faQuestion} size="10x" />
+                    <br />
+                    <br />
+                    <ErrorTitle>404 페이지없음!</ErrorTitle>
+                    <h1>페이지를 찾을 수 없어요</h1>
+                    <h3>주소가 잘못되거나 존재하지 않는 페이지이에요</h3>
+                </WarningStyle>
+            </CenterContainer>
+            <Footer />
+        </ErrorContainer>
     );
 };
 
