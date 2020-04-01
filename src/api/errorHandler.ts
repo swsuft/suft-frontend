7c8cdc59c6b28936afdaf929aee786beaafa6c7c
SkyLightQP <combbm@naver.com>
2020-04-01 22:54:54 +0900

    fix: fix serverErrorHandler() parameters

--- src/api/errorHandler.ts
+++ src/api/errorHandler.ts
@@ -1,9 +1,9 @@
 import { AxiosError } from 'axios';
 import Error from '../error/Error';
 
-const serverErrorHandler = (error: Error) => {
+const serverErrorHandler = (code: Error, message: string) => {
     alert('서버 오류가 발생하였습니다. 잠시후 다시 시도해주세요.\n문제가 지속될 경우 관리자에게 알려주세요.');
-    console.log(`오류: ${error}`);
+    console.log('ERROR', code, message);
 };
 
 // eslint-disable-next-line import/prefer-default-export
@@ -23,7 +23,7 @@ export const DefaultErrorHandler = (err: AxiosError) => {
     }
 
     if (code === Error.SERVER_ERROR) {
-        serverErrorHandler(err);
+        serverErrorHandler(code, message);
         return;
     }
 
