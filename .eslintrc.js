8e67d062ae98dcb42ab315597c62e65ea50185f0
SkyLightQP <combbm@naver.com>
2020-04-20 23:43:39 +0900

    chore(eslint): fix eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,24 @@
 module.exports = {
-    "extends": [
-        "react-app",
-        "airbnb"
-    ],
-    "plugins": [
-        "react-hooks"
-    ],
-    "rules": {
-        "arrow-body-style": "off",
-        "indent": "off",
-        "jsx-quotes": "off",
-        "comma-dangle": "off",
-        "object-curly-newline": "off",
-        "import/no-unresolved": "off",
-        "max-len": "off",
-        "react/jsx-indent": "off",
-        "react/jsx-filename-extension": "off",
-        "react/no-unescaped-entities": "off",
-        "react/jsx-tag-spacing": "off",
-        "react/prop-types": "off",
-        "react/jsx-one-expression-per-line": "off",
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "import/extensions": "off",
-        "no-alert": "off",
-        "no-console": "off"
+    extends: ['react-app', 'airbnb'],
+    plugins: ['react-hooks'],
+    rules: {
+        'arrow-body-style': 'off',
+        'indent': 'off',
+        'jsx-quotes': 'off',
+        'comma-dangle': 'off',
+        'object-curly-newline': 'off',
+        'import/no-unresolved': 'off',
+        'max-len': 'off',
+        'react/jsx-indent': 'off',
+        'react/jsx-filename-extension': 'off',
+        'react/no-unescaped-entities': 'off',
+        'react/jsx-tag-spacing': 'off',
+        'react/prop-types': 'off',
+        'react/jsx-one-expression-per-line': 'off',
+        'linebreak-style': ['error', 'windows'],
+        'import/extensions': 'off',
+        'no-alert': 'off',
+        'no-console': 'off',
+        'import/no-extraneous-dependencies': 'off'
     }
 };
