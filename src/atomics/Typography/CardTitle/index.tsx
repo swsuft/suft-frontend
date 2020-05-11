10ef762b8e87f0a7850cb0c1f81a466c6c925c72
SkyLightQP <combbm@naver.com>
2020-05-11 23:34:34 +0900

    fix: remove margin-bottom style

--- src/atomics/Typography/CardTitle/index.tsx
+++ src/atomics/Typography/CardTitle/index.tsx
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 const TextStyle = styled.p`
     font-size: 26px;
     font-family: 'Godo', sans-serif;
-    margin-bottom: 0.5rem;
 `;
 
 const CardTitle: React.FC = ({ children }) => <TextStyle>{children}</TextStyle>;
