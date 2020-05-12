539c4b4b8f5c4b5c80f2760f5a5db1c31fa1bc44
SkyLightQP <combbm@naver.com>
2020-05-12 14:15:52 +0900

    fix: change margin value

--- src/atomics/Typography/FontedTitle/index.tsx
+++ src/atomics/Typography/FontedTitle/index.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const FontedTitle = styled.p`
     font-family: 'Godo', sans-serif;
     font-size: 1.8rem;
-    margin: 2rem 0;
+    margin: 1.5rem 0;
 `;
 
 export default FontedTitle;
