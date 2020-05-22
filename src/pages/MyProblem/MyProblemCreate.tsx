64a606f8e85efc3defba0b97ebaa9e9379139804
SkyLightQP <combbm@naver.com>
2020-05-22 13:51:51 +0900

    feat: implement MyProblemView page

--- src/pages/MyProblem/MyProblemCreate.tsx
+++ src/pages/MyProblem/MyProblemCreate.tsx
@@ -1,6 +1,9 @@
 import React, { useRef, useState } from 'react';
 import { Editor } from '@toast-ui/react-editor';
 import styled from 'styled-components';
+import cogoToast from 'cogo-toast';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 import FontedTitle from '../../atomics/Typography/FontedTitle';
 import ProblemEditor from '../../components/ProblemEditor';
 import MyProblemLayout from '../../layouts/MyProblemLayout';
@@ -10,6 +13,9 @@ import GradeOption from '../../atomics/SelectOptions/GradeOption';
 import TimesOption from '../../atomics/SelectOptions/TimesOption';
 import SmallButton from '../../atomics/SmallButton';
 import DynamicSubject from '../../utils/DynamicSubject';
+import ProblemApi from '../../api/Problem';
+import { useProfile } from '../../hooks/useProfile';
+import useToken from '../../hooks/useToken';
 
 const SmallSelectStyle = styled(SmallSelect)`
     width: 100px;
@@ -20,12 +26,42 @@ const SmallButtonStyle = styled(SmallButton)`
 `;
 
 const MyProblemCreate: React.FC = () => {
-    const editorRef = useRef<Editor>();
+    const profile = useProfile();
+    const refreshToken = useToken();
 
+    const editorRef = useRef<Editor>();
     const [answer, setAnswer] = useState<string>('');
     const [grade, setGrade] = useState<string>('');
     const [times, setTimes] = useState<string>('');
     const [subject, setSubject] = useState<string>('');
+
+    const createProblem = () => {
+        if (!editorRef.current) return;
+        if (!profile) return;
+
+        if (answer === '' || subject === '' || grade === '' || times === '') {
+            cogoToast.warn('빈 칸이 있습니다.');
+            return;
+        }
+
+        const html = editorRef.current.getInstance().getHtml();
+
+        const problemData = {
+            email: profile.data!!.email,
+            author: profile.data!!.name,
+            contents: html,
+            answer,
+            subject,
+            grade,
+            times
+        };
+
+        ProblemApi.create(problemData).then(() => {
+            cogoToast.success('성공적으로 문제를 등록하였습니다.');
+        });
+
+        refreshToken();
+    };
 
     return (
         <MyProblemLayout>
@@ -61,7 +97,9 @@ const MyProblemCreate: React.FC = () => {
                     <DynamicSubject current={grade} />
                 </SmallSelect>
                 <br />
-                <SmallButtonStyle background="var(--color-blue)">등록</SmallButtonStyle>
+                <SmallButtonStyle background="var(--color-blue)" onClick={createProblem}>
+                    <FontAwesomeIcon icon={faPencilAlt} /> 등록하기
+                </SmallButtonStyle>
             </div>
             <br />
         </MyProblemLayout>
