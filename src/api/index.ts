0dd63017aff0b6003286b1a41dcc00450a121314
SkyLightQP <combbm@naver.com>
2020-07-08 00:17:45 +0900

    feat: add apollo-client in api for graphql

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,28 @@
 import axios, { AxiosInstance } from 'axios';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import config from '../config';
 import TokenUtil from './TokenUtil';
+
+const httpLink = createHttpLink({
+    uri: config.API_URL
+});
+
+const authLink = setContext((_, { headers }) => {
+    return {
+        headers: {
+            ...headers,
+            authorization: TokenUtil.isEmpty() ? '' : `Bearer ${TokenUtil.get()}`
+        }
+    };
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+});
 
 const getAxiosInstance = (): AxiosInstance => {
     const instance = axios.create({
@@ -14,6 +36,5 @@ const getAxiosInstance = (): AxiosInstance => {
     return instance;
 };
 
-const Api = getAxiosInstance();
-
-export default Api;
+export const Api = getAxiosInstance();
+export const Apollo = client;
