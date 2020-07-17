11a42d832773d143f4cb78f7b02ecc125bc3a94d
SkyLightQP <combbm@naver.com>
2020-07-18 00:59:40 +0900

    refactor: implement useToken() with graphql
    - refactor: implement login view with graphql

--- src/hooks/useToken.ts
+++ src/hooks/useToken.ts
@@ -1,21 +1,30 @@
 import { useCallback } from 'react';
+import { gql } from 'apollo-boost';
+import { useMutation } from '@apollo/react-hooks';
+import cogoToast from 'cogo-toast';
 import TokenUtil from '../api/TokenUtil';
-import AuthApi from '../api/Auth';
+
+const GET_TOKEN = gql`
+    mutation {
+        token
+    }
+`;
 
 const useToken = () => {
-    const refreshToken = useCallback(() => {
-        if (TokenUtil.isEmpty()) {
+    const [, { loading, error, data }] = useMutation(GET_TOKEN);
+
+    return useCallback(() => {
+        if (loading) return;
+
+        if (error) {
+            cogoToast.error('로그인 연장 중 오류가 발생하였습니다. 로그아웃 후 다시 시도하세요.');
             return;
         }
 
-        AuthApi.token().then((res) => {
-            TokenUtil.set(res.data.token);
-            window.location.reload();
-            console.log('AccessToken 재발급 완료');
-        });
-    }, []);
+        if (TokenUtil.isEmpty()) return;
 
-    return refreshToken;
+        console.log(data);
+    }, [loading, error, data]);
 };
 
 export default useToken;
