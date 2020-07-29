6f5be0846d10676e504b578ea6d3966650c00bf9
SkyLightQP <combbm@naver.com>
2020-07-29 17:26:07 +0900

    refactor: implement AdminWaitingUser view with graphql

--- src/pages/Admin/AdminWaitingUser.tsx
+++ src/pages/Admin/AdminWaitingUser.tsx
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import cogoToast from 'cogo-toast';
 import styled from 'styled-components';
+import { gql } from 'apollo-boost';
+import { useMutation, useQuery } from '@apollo/react-hooks';
 import FontedTitle from '../../atomics/Typography/FontedTitle';
 import AdminLayout from '../../layouts/AdminLayout';
 import Table from '../../components/Table';
 import useSelect from '../../hooks/useSelect';
-import WaitingApi from '../../api/Waiting';
+import useToken from '../../hooks/useToken';
+import { getGraphQLError } from '../../api/errorHandler';
+import ErrorCode from '../../error/ErrorCode';
 
 const TableWrap = styled.div`
     margin-bottom: 1rem;
@@ -37,19 +41,69 @@ const DenyButtonStyle = styled.button`
     cursor: pointer;
 `;
 
+const GET_WAITINGUSER = gql`
+    query {
+        waitingUsers {
+            email
+            grade
+            name
+            isDeny
+            createdAt
+        }
+    }
+`;
+
+const ALLOW_USER = gql`
+    mutation($email: String!) {
+        allowUser(email: $email) {
+            email
+        }
+    }
+`;
+
+const DENY_USER = gql`
+    mutation($email: String!) {
+        denyUser(email: $email) {
+            email
+        }
+    }
+`;
+
 const AdminWaitingUser: React.FC = () => {
-    const [data, setData] = useState<[]>([]);
+    const refreshToken = useToken();
+
+    const [waitingUserData, setWaitingUserData] = useState<[]>([]);
     const [check, rowManager] = useSelect();
 
-    const refreshWaitingUserList = () => {
-        WaitingApi.all().then((res) => {
-            setData(res.data.data);
-        });
-    };
+    const { loading, error, data, refetch: refatchWaitingUser } = useQuery(GET_WAITINGUSER);
+    const [allowUserQurey] = useMutation(ALLOW_USER);
+    const [denyUserQurey] = useMutation(DENY_USER);
+
+    const refreshWaitingUserList = useCallback(() => {
+        if (loading) {
+            cogoToast.loading('가입 요청 목록을 가져오고 있어요...', {
+                hideAfter: 1
+            });
+            return;
+        }
+
+        if (error) {
+            const gerror = getGraphQLError(error);
+            if (!gerror) return;
+
+            if (gerror[0] === ErrorCode.NO_PERMISSION) {
+                refreshToken();
+            } else {
+                cogoToast.error(gerror[1]);
+            }
+        }
+
+        setWaitingUserData(data.waitingUsers);
+    }, [loading, error, data, refreshToken]);
 
     useEffect(() => {
         refreshWaitingUserList();
-    }, []);
+    }, [refreshWaitingUserList]);
 
     const allowUsers = () => {
         const { selected } = check;
@@ -60,21 +114,37 @@ const AdminWaitingUser: React.FC = () => {
         }
 
         const isReal = window.confirm(
-            `${Object.keys(selected).length}명을 가입 수락 하시겠습니까?\n가입 수락은 되돌릴 수 없습니다.`
+            `${Object.keys(selected).length}명을 가입 수락 하시겠습니까? 가입 수락은 되돌릴 수 없습니다.`
         );
 
         if (!isReal) return;
 
-        Object.keys(selected).forEach((key: string) => {
-            if (!selected[key]) return;
+        const allowPromise = Object.keys(selected).map((key: string) => {
+            return new Promise((resolve, reject) => {
+                if (!selected[key]) reject();
 
-            WaitingApi.allow(key).then(() => {
-                refreshWaitingUserList();
-                rowManager.uncheckAllRow();
+                allowUserQurey({
+                    variables: {
+                        email: key
+                    }
+                })
+                    .then(() => {
+                        resolve();
+                    })
+                    .catch((err) => {
+                        const gerror = getGraphQLError(err);
+                        if (!gerror) return;
+                        reject(gerror[1]);
+                    });
             });
         });
 
-        cogoToast.success(`${Object.keys(selected).length}명 가입 수락 완료`);
+        Promise.all(allowPromise).then(async () => {
+            rowManager.uncheckAllRow();
+            const { data: newData } = await refatchWaitingUser();
+            setWaitingUserData(newData.waitingUsers);
+            cogoToast.success(`${Object.keys(selected).length}명 가입 수락 완료`);
+        });
     };
 
     const denyUsers = () => {
@@ -89,16 +159,32 @@ const AdminWaitingUser: React.FC = () => {
 
         if (!isReal) return;
 
-        Object.keys(selected).forEach((key: string) => {
-            if (!selected[key]) return;
+        const denyPromise = Object.keys(selected).map((key: string) => {
+            return new Promise((resolve, reject) => {
+                if (!selected[key]) reject();
 
-            WaitingApi.deny(key).then(() => {
-                refreshWaitingUserList();
-                rowManager.uncheckAllRow();
+                denyUserQurey({
+                    variables: {
+                        email: key
+                    }
+                })
+                    .then(() => {
+                        resolve();
+                    })
+                    .catch((err) => {
+                        const gerror = getGraphQLError(err);
+                        if (!gerror) return;
+                        reject(gerror[1]);
+                    });
             });
         });
 
-        cogoToast.success(`${Object.keys(selected).length}명 가입 거절 완료`);
+        Promise.all(denyPromise).then(async () => {
+            rowManager.uncheckAllRow();
+            const { data: newData } = await refatchWaitingUser();
+            setWaitingUserData(newData.waitingUsers);
+            cogoToast.success(`${Object.keys(selected).length}명 가입 거절 완료`);
+        });
     };
 
     const columns = [
@@ -117,7 +203,7 @@ const AdminWaitingUser: React.FC = () => {
                                     input.indeterminate = check.selectAll === 2;
                                 }
                             }}
-                          onChange={() => rowManager.toggleAllRow(data, 'email')}
+                          onChange={() => rowManager.toggleAllRow(waitingUserData, 'email')}
                         />
                     </CheckboxWrapStyle>
                 );
@@ -158,7 +244,10 @@ const AdminWaitingUser: React.FC = () => {
         },
         {
             Header: '가입 요청일',
-            accessor: 'createdAt'
+            accessor: 'createdAt',
+            Cell: ({ row }: any) => {
+                return new Date(parseInt(row.original.createdAt, 10)).toLocaleDateString();
+            }
         }
     ];
 
@@ -171,7 +260,7 @@ const AdminWaitingUser: React.FC = () => {
             </div>
 
             <TableWrap>
-                <Table columns={columns} data={data} />
+                <Table columns={columns} data={waitingUserData} />
             </TableWrap>
         </AdminLayout>
     );
